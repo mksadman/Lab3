dfe54826905651e17bdbdfd267cabdd4eab0ecb4
Md. Sadman Mubasshir Khan <sadmankhan@iut-dhaka.edu>
2024-09-25 22:22:53 +0600

    Completed Task 2

--- src/main/java/task2/Tests/TestMinStack.java
+++ src/main/java/task2/Tests/TestMinStack.java
@@ -1,4 +1,20 @@
 package task2.Tests;
 
+import task2.main.MinStack;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class TestMinStack {
+    public void testMinStack(){
+        MinStack minStack = new MinStack();
+        minStack.push(3);
+        minStack.push(5);
+        minStack.push(2);
+        minStack.push(1);
+        assertEquals(1, minStack.getMin());
+        minStack.pop();
+        assertEquals(2, minStack.getMin());
+        minStack.pop();
+        assertEquals(3, minStack.getMin());
+    }
 }
--- src/main/java/task2/main/MinStack.java
+++ src/main/java/task2/main/MinStack.java
@@ -1,4 +1,60 @@
 package task2.main;
 
+import java.util.Stack;
+
 public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+        System.out.println("Pushed " + x + " to stack");
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) {
+            return;
+        }
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public void min(){
+        if (minStack.isEmpty()){
+            System.out.println("Stack is empty");
+        }
+        else {
+            System.out.println("Minimum element in the stack is: " + minStack.peek());
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack ms = new MinStack();
+        ms.push(5);
+        ms.push(60);
+        ms.push(165);
+        ms.push(90);
+        ms.push(10);
+        ms.min();
+        ms.pop();
+        ms.min();
+    }
 }
